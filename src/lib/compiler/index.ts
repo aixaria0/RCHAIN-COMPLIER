a85da645610ebfe5fae8be03403e10982409276f
AixAria <mailboxofaria@gmail.com>
2026-09-17 23:32:43 +0330

    feat: expose Reality Record primitives

--- src/lib/compiler/index.ts
+++ src/lib/compiler/index.ts
@@ -2,4 +2,5 @@ export * from "./types";
 export * from "./hash";
 export * from "./qlf";
 export * from "./compile";
+export * from "./reality-record";
 export { shortHex, hexPrefixed } from "./hash";
