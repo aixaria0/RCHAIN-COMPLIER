e4d23851a2fafa72dc102211729ae9cfa1b85be3
AixAria <mailboxofaria@gmail.com>
2026-09-17 23:34:53 +0330

    feat: export Reality Record compiler adapter

--- src/lib/compiler/index.ts
+++ src/lib/compiler/index.ts
@@ -3,4 +3,5 @@ export * from "./hash";
 export * from "./qlf";
 export * from "./compile";
 export * from "./reality-record";
+export * from "./reality-record-adapter";
 export { shortHex, hexPrefixed } from "./hash";
