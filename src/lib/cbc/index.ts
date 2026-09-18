57d8041ddd4f39b793e3ca89593cc173c442bcbe
AixAria <mailboxofaria@gmail.com>
2026-09-19 00:48:12 +0330

    feat: add upstream Casper finality law probe

--- src/lib/cbc/index.ts
+++ src/lib/cbc/index.ts
@@ -3,4 +3,5 @@ export * from "./network-partition.ts";
 export * from "./equivocation-engine.ts";
 export * from "./cbc-simulator.ts";
 export * from "./casper-observation.ts";
+export * from "./casper-finality.ts";
 export * from "./fragility-engine.ts";
