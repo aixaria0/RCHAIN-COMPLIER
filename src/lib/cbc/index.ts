d2725174a5aada2a6e77ebea0c8c98181618c471
AixAria <mailboxofaria@gmail.com>
2026-09-19 00:40:02 +0330

    feat: export CBC fragility engine

--- src/lib/cbc/index.ts
+++ src/lib/cbc/index.ts
@@ -3,3 +3,4 @@ export * from "./network-partition.ts";
 export * from "./equivocation-engine.ts";
 export * from "./cbc-simulator.ts";
 export * from "./casper-observation.ts";
+export * from "./fragility-engine.ts";
