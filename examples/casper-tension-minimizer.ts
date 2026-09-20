82138cd45b013fb7af19d5b0b9a1fcd66ff2fb64
AixAria <mailboxofaria@gmail.com>
2026-09-20 19:03:41 +0330

    fix(cbc): use trace bond field in minimizer demo

--- examples/casper-tension-minimizer.ts
+++ examples/casper-tension-minimizer.ts
@@ -8,15 +8,15 @@ const result = minimizeTensionObservation({
 
 console.log(JSON.stringify({
   original: {
-    validators: Object.keys(result.original.bondsMap).length,
+    validators: Object.keys(result.original.bonds).length,
     supportingStake: result.original.supportingStake,
     totalStake: result.original.totalStake,
     messageCoverage: result.original.messageCoverage,
     digest: result.original.observationDigest,
   },
   minimized: {
-    validators: Object.keys(result.minimized.bondsMap).length,
-    bonds: result.minimized.bondsMap,
+    validators: Object.keys(result.minimized.bonds).length,
+    bonds: result.minimized.bonds,
     support: result.minimized.support,
     minimumMessageSenders: result.minimized.minimumMessageSenders,
     supportingStake: result.minimized.supportingStake,
